95c8a640c00ff696b241dd37e56a193e85791e77
Eyal Lotem <eyal.lotem@gmail.com>
2021-05-07 00:06:10 +0300

    Add a richer default env type

--- src/GUI/Momentu/DefaultEnv.hs
+++ src/GUI/Momentu/DefaultEnv.hs
@@ -14,6 +14,7 @@ import qualified GUI.Momentu.Main as Main
 import           GUI.Momentu.State (HasCursor, GUIState)
 import qualified GUI.Momentu.Widgets.Choice as Choice
 import qualified GUI.Momentu.Widgets.EventMapHelp as EventMapHelp
+import qualified GUI.Momentu.Widgets.TextEdit as TextEdit
 import qualified GUI.Momentu.Widgets.TextView as TextView
 import qualified GUI.Momentu.Zoom as Zoom
 
@@ -24,6 +25,7 @@ data DefaultEnvG guiState = DefaultEnv
     { _commonTexts :: I18N.Texts Text
     , _mainTexts :: Main.Texts Text
     , _choiceTexts :: Choice.Texts Text
+    , _textEditTexts :: TextEdit.Texts Text
     , _directionTexts :: Direction.Texts Text
     , _directionLayout :: Direction.Layout
     , _zoomTexts :: Zoom.Texts Text
@@ -33,6 +35,7 @@ data DefaultEnvG guiState = DefaultEnv
     , _animIdPrefix :: AnimId
     , _helpConfig :: EventMapHelp.Config
     , _helpStyle :: EventMapHelp.Style
+    , _textEditStyle :: TextEdit.Style
     , _textViewStyle :: TextView.Style
     , _hoverStyle :: Hover.Style
     , _guiState :: guiState
@@ -48,6 +51,7 @@ defaultEnv font = DefaultEnv
     { _commonTexts = I18N.englishTexts
     , _mainTexts = Main.englishTexts
     , _choiceTexts = Choice.englishTexts
+    , _textEditTexts = TextEdit.englishTexts
     , _directionTexts = Direction.englishTexts
     , _directionLayout = Direction.LeftToRight
     , _zoomTexts = Zoom.englishTexts
@@ -57,6 +61,7 @@ defaultEnv font = DefaultEnv
     , _animIdPrefix = []
     , _helpConfig = EventMapHelp.defaultConfig
     , _helpStyle = EventMapHelp.defaultStyle font
+    , _textEditStyle = TextView.whiteText font & TextEdit.defaultStyle
     , _textViewStyle = TextView.whiteText font
     , _hoverStyle = Hover.defaultStyle
     , _guiState = ()
@@ -66,18 +71,20 @@ defaultEnvWithCursor :: GUIState -> Font -> DefaultEnvWithCursor
 defaultEnvWithCursor state font = defaultEnv font & guiState .~ state
 
 instance Element.HasAnimIdPrefix       (DefaultEnvG a) where animIdPrefix = animIdPrefix
+instance Has (Choice.Texts Text)       (DefaultEnvG a) where has = choiceTexts
 instance Has (Direction.Texts Text)    (DefaultEnvG a) where has = directionTexts
 instance Has (EventMap.Texts Text)     (DefaultEnvG a) where has = eventMapTexts
 instance Has (EventMapHelp.Texts Text) (DefaultEnvG a) where has = helpTexts
 instance Has (Glue.Texts Text)         (DefaultEnvG a) where has = glueTexts
 instance Has (I18N.Texts Text)         (DefaultEnvG a) where has = commonTexts
 instance Has (Main.Texts Text)         (DefaultEnvG a) where has = mainTexts
-instance Has (Choice.Texts Text)       (DefaultEnvG a) where has = choiceTexts
+instance Has (TextEdit.Texts Text)     (DefaultEnvG a) where has = textEditTexts
 instance Has (Zoom.Texts Text)         (DefaultEnvG a) where has = zoomTexts
 instance Has Direction.Layout          (DefaultEnvG a) where has = directionLayout
 instance Has EventMapHelp.Config       (DefaultEnvG a) where has = helpConfig
 instance Has EventMapHelp.Style        (DefaultEnvG a) where has = helpStyle
-instance Has TextView.Style            (DefaultEnvG a) where has = textViewStyle
 instance Has Hover.Style               (DefaultEnvG a) where has = hoverStyle
+instance Has TextEdit.Style            (DefaultEnvG a) where has = textEditStyle
+instance Has TextView.Style            (DefaultEnvG a) where has = textViewStyle
 instance Has GUIState                  DefaultEnvWithCursor where has = guiState
 instance HasCursor                     DefaultEnvWithCursor
