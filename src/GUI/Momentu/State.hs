lamdu/momentu

commit d423ef550034f5178d5888e254ddb63df1062a84

Transition from convention of parameters inside reader-monad result.
Not sure why we used that convention to start with.
With monads other than readers it does convey a meaning of actions not depending on inner args,
but with reader this case is moot.

src/GUI/Momentu/State.hs
@@ -77,11 +77,11 @@ class HasCursor env where
 
 instance HasCursor GUIState where cursor = sCursor
 
-subId :: (MonadReader env m, HasCursor env) => m (ElemId -> Maybe ElemId)
-subId = Lens.view cursor <&> flip ElemId.subId
+subId :: (MonadReader env m, HasCursor env) => ElemId -> m (Maybe ElemId)
+subId i = Lens.view cursor <&> (`ElemId.subId` i)
 
-isSubCursor :: (MonadReader env m, HasCursor env) => m (ElemId -> Bool)
-isSubCursor = subId <&> \sub prefix -> sub prefix & Lens.has Lens._Just
+isSubCursor :: (MonadReader env m, HasCursor env) => ElemId -> m Bool
+isSubCursor i = subId i <&> Lens.has Lens._Just
 
 assignCursor ::
     (HasCursor env, MonadReader env m) =>

examples/dropdownlist.hs
@@ -39,8 +39,8 @@ makeWidget dropDownListRef _getFont env =
     do
         prop <- Property.fromIORef dropDownListRef ^. Property.mkProperty
         let dropDownListWidget =
-                DropDownList.make env prop (map makeDropDownList colors)
-                (DropDownList.defaultConfig env "Color") mempty
+                DropDownList.make prop (colors <&> makeDropDownList)
+                (DropDownList.defaultConfig "Color" env) mempty env
                 ^. M.tValue
         let Just color = lookup (Property.value prop) colors
         let box =

examples/hello-world.hs
@@ -19,7 +19,7 @@ main =
 
 makeWidget :: (Float -> IO M.Font) -> M.DefaultEnvWithCursor -> IO (Widget IO)
 makeWidget _getFont env =
-    TextView.make env "Hello World!" "hello"
+    TextView.make "Hello World!" "hello" env
     ^. M.tValue
     & Widget.fromView
     & Widget.setFocused

examples/searchmenu.hs
@@ -59,14 +59,11 @@ resultId = "result"
 makeResultWidget ::
     Applicative f => Text -> M.DefaultEnvWithCursor -> M.WithTextPos (Widget.Widget f)
 makeResultWidget text =
-    do
-        toFocusable <- Widget.makeFocusableView
-        TextView.make ?? text ?? resultId <&> M.tValue %~ toFocusable resultId
+    TextView.make text resultId >>= M.tValue (Widget.makeFocusableView resultId)
 
 makeMenu :: Applicative f => (Text -> f M.ElemId) -> [Text] -> M.ElemId -> M.DefaultEnvWithCursor -> M.TextWidget f
 makeMenu pickText opts menuId =
-    SearchMenu.make makeSearchTerm makeResults Element.empty menuId
-    ?? SearchMenu.AnyPlace
+    SearchMenu.make makeSearchTerm makeResults Element.empty menuId SearchMenu.AnyPlace
     where
         makeSearchTerm = SearchMenu.searchTermEdit menuId (const (pure True))
         makeResults (SearchMenu.ResultsContext searchTerm resultIdPrefix) _ =
@@ -91,7 +88,7 @@ makeMenu pickText opts menuId =
                     , Widget._pTextRemainder = ""
                     }
                 , SearchMenu._rWidget =
-                    Label.make text e <&> Widget.makeFocusableView e widgetId
+                    (Label.make text >>= traverse (Widget.makeFocusableView widgetId)) e
                 }
             }
             where

examples/textedit.hs
@@ -48,12 +48,8 @@ mkTextEdit ::
     IORef ioref -> Text -> Text -> M.ElemId -> Lens.ALens' ioref Text ->
     IO (env -> Widget.Widget IO)
 mkTextEdit textRef uempty fempty myId alens =
-    readIORef textRef
-    <&> (^# alens)
-    <&> \curText localEnv ->
-    TextEdit.make localEnv (TextEdit.Modes uempty fempty)
-    curText myId
+    readIORef textRef <&> (^# alens) <&>
+    \curText localEnv ->
+    TextEdit.make (TextEdit.Modes uempty fempty) curText myId localEnv
     & (^. M.tValue)
-    & Widget.updates %~
-    \(newText, update) ->
-        update <$ modifyIORef textRef (alens #~ newText)
+    & Widget.updates %~ \(newText, update) -> update <$ modifyIORef textRef (alens #~ newText)

momentu.cabal
@@ -5,7 +5,7 @@ cabal-version: 1.12
 -- see: https://github.com/sol/hpack
 
 name:           momentu
-version:        0.1.3.0
+version:        0.1.4.0
 synopsis:       Purely functional animated GUI framework
 description:    Purely functional animated GUI framework
 category:       Graphics
@@ -26,6 +26,7 @@ source-repository head
 library
   exposed-modules:
       Control.Concurrent.Extended
+      Control.Monad.Reader.Extended
       Data.Aeson.TH.Extended
       Data.Binary.Extended
       Data.ByteString.Extended

package.yaml
@@ -1,5 +1,5 @@
 name: momentu
-version: 0.1.3.0
+version: 0.1.4.0
 synopsis: Purely functional animated GUI framework
 description: Purely functional animated GUI framework
 github: "lamdu/momentu"

src/Control/Monad/Reader/Extended.hs
@@ -1,0 +1,26 @@
+module Control.Monad.Reader.Extended
+    ( module Control.Monad.Reader
+    , pushToReader, pushToReaderExt
+    ) where
+
+import           Control.Monad.Reader
+
+import           Prelude
+
+-- | With the reader monad,
+-- whether a function parameter is inside the monadic action's result,
+-- or whether it was given outside of it, is equivalent!
+--
+-- It is usually more ergonomic for the parameter to be outside,
+-- but in some case one needs to move a parameter inside.
+--
+-- This is the transformation that @pushToReader@ performs.
+-- Its opposite is the @Control.Lens.(??)@ operator.
+pushToReader :: MonadReader env m => (a -> env -> r) -> m (a -> r)
+pushToReader = asks . flip
+
+-- | Extend @pushToReader@ for more arguments.
+--
+-- 'pushToReaderExt pushToReader :: MonadReader env m => (a -> b -> env -> r) -> m (a -> b -> r)'
+pushToReaderExt :: MonadReader env m => (b -> env -> r) -> (a -> b) -> m (a -> r)
+pushToReaderExt f = pushToReader . fmap f

commit 01d6a63eeb81b328471930071f07d95759111a6f

Fix red cursor bug introduced in d423ef550034
Repro: in textedit example type something or move cursor

src/GUI/Momentu/State.hs
@@ -78,7 +78,7 @@ class HasCursor env where
 instance HasCursor GUIState where cursor = sCursor
 
 subId :: (MonadReader env m, HasCursor env) => ElemId -> m (Maybe ElemId)
-subId i = Lens.view cursor <&> (`ElemId.subId` i)
+subId i = Lens.view cursor <&> ElemId.subId i
 
 isSubCursor :: (MonadReader env m, HasCursor env) => ElemId -> m Bool
 isSubCursor i = subId i <&> Lens.has Lens._Just